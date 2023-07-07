018fb38a44e485acf0e287d0d4481e36277781b7
jhoon9494 <wjdgns9406@gmail.com>
2023-07-07 10:59:15 +0900

    Feat: Router, Route 컴포넌트 구현

---
+++ src/components/Router.tsx
@@ -0,0 +1,36 @@
+import { Dispatch, ReactElement, SetStateAction, createContext, useEffect, useState } from 'react';
+import NotFound from './NotFound';
+
+type RouterContextType = null | { path: string; setPath: Dispatch<SetStateAction<string>> };
+
+export const RouterContext = createContext<RouterContextType>(null);
+
+export const Router = ({ children }: { children: ReactElement[] }) => {
+  const [path, setPath] = useState(window.location.pathname);
+
+  const handlePopstate = () => {
+    setPath(window.location.pathname);
+  };
+
+  useEffect(() => {
+    window.addEventListener('popstate', handlePopstate);
+    return () => {
+      handlePopstate();
+    };
+  }, []);
+
+  const renderChild = children.find((c) => c.props.path === window.location.pathname);
+
+  if (!renderChild) return <NotFound />;
+
+  return <RouterContext.Provider value={{ path, setPath }}>{renderChild}</RouterContext.Provider>;
+};
+
+interface RouteProps {
+  path: string;
+  element: ReactElement;
+}
+
+export const Route = ({ element }: RouteProps) => {
+  return <>{element}</>;
+};
